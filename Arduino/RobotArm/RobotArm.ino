df82718e57a217fc23cd6858c7eef5082eb62e65
Brad W <thatbr4d@gmail.com>
2021-07-15 14:44:18 -0400

    fully functional rotation/movement/claw with IR remote

--- Arduino/RobotArm/RobotArm.ino
+++ Arduino/RobotArm/RobotArm.ino
@@ -6,78 +6,101 @@
 #define IRLeft 3141861120
 #define IRRight 3158572800
 #define IRStop 3208707840
+#define IRUp 3108437760
+#define IRDown 3927310080
+#define IRClose 4161273600
+#define IROpen 4127850240
 #define IRPin 3
 
 /** Motor Constants **/
 #define motorSpeedPin 5
 #define motorDir1Pin 4
 #define motorDir2Pin 2
-#define motorSpeed 150
+#define motorSpeed 200
 
-IRrecv irrecv(IRPin);
+/** Arm and Claw Constants **/
+#define armPin 9
+#define clawPin 10
+#define armPositionDown 1000
+#define armPositionUp 1500
+#define clawPositionOpen 1000
+#define clawPositionClosed 1925
 
 /** All movements the robot is capable of **/
 enum controls{
   NONE,
   LEFT,
-  RIGHT
+  RIGHT,
+  UP,
+  DOWN,
+  OPEN,
+  CLOSE
 };
-controls currentControl;
 
-int longArmPin = 9;
-int clawPin = 12;
-
-Servo longArmServo;
+IRrecv irrecv(IRPin);
+Servo armServo;
 Servo clawServo;
 
-int longArmPositionGround = 1000;
-int longArmPositionUp = 1500;
-int clawPositionOpen = 1000;
-int clawPositionClosed = 1925;
+controls currentControl;
+int currentArmPosition;
+int currentClawPosition;
 
 void setup() {
-  
   Serial.begin(9600);
   irrecv.enableIRIn();
   pinMode(motorSpeedPin, OUTPUT);
   pinMode(motorDir1Pin, OUTPUT);
   pinMode(motorDir2Pin, OUTPUT);
-  longArmServo.attach(longArmPin);
+  armServo.attach(armPin);
   clawServo.attach(clawPin);
-  
+
+  //set initial positions
+  returnUp();
+  closeClaw();
 }
 
 void loop() {
 
   handleIRInput(&currentControl);
   rotate(currentControl);
-  
-  /*openClaw();
-  goDown();
-  closeClaw();
-  returnUp();*/
-  
+  moveArm(currentControl);
+  openCloseClaw(currentControl);
+   
 }
 
-void handleIRInput(controls *currentControl){
-  
+void handleIRInput(controls *current){
   if(irrecv.decode()){
     if(irrecv.decodedIRData.decodedRawData != IRRepeat){
-      if(irrecv.decodedIRData.decodedRawData == IRLeft)
-        *currentControl = LEFT;
-      else if(irrecv.decodedIRData.decodedRawData == IRRight)
-        *currentControl = RIGHT;
-      else if(irrecv.decodedIRData.decodedRawData == IRStop)
-        *currentControl = NONE;
+      switch(irrecv.decodedIRData.decodedRawData){
+        case IRLeft:
+          *current = LEFT;
+          break;
+        case IRRight:
+          *current = RIGHT;
+          break;
+        case IRStop:
+          *current = NONE;
+          break;
+        case IRUp:
+          *current = UP;
+          break;
+        case IRDown:
+          *current = DOWN;
+          break;
+        case IROpen:
+          *current = OPEN;
+          break;
+        case IRClose:
+          *current = CLOSE;
+          break;
+      }
     }
 
     irrecv.resume();
   }
-  
 }
 
 void rotate(controls current){
-  Serial.println(current);
   switch(current){
     case LEFT:
       digitalWrite(motorDir1Pin, HIGH);
@@ -93,33 +116,80 @@ void rotate(controls current){
       analogWrite(motorSpeedPin, 0);
       break;
   }
-  
+}
+
+void moveArm(controls current){
+  switch(current){
+    case UP:
+      addArmIncrement(currentArmPosition + 1, 1);
+      break;
+    case DOWN:
+      addArmIncrement(currentArmPosition - 1, 5);
+      break;
+    default:
+      
+      break;
+  }
+}
+
+void openCloseClaw(controls current){
+  switch(current){
+    case OPEN:
+      addClawIncrement(currentClawPosition - 1);
+      break;
+    case CLOSE:
+      addClawIncrement(currentClawPosition + 1);
+      break;
+    default:
+      
+      break;
+  }
+}
+
+void addArmIncrement(int increment, int delayTime){
+   if(increment > armPositionUp || increment < armPositionDown)
+    return;
+      
+   armServo.writeMicroseconds(increment);
+   currentArmPosition = increment;
+   delay(delayTime);
 }
 
 void goDown(){
-  for(int i = longArmPositionUp; i > longArmPositionGround; i--){
-    longArmServo.writeMicroseconds(i);
-    delay(5);
+  for(int i = armPositionUp; i > armPositionDown; i--){
+    addArmIncrement(i, 5);
   }
 }
 
 void returnUp(){
-  for(int i = longArmPositionGround; i < longArmPositionUp; i++){
-    longArmServo.writeMicroseconds(i);
-    delay(1);
+  if(currentArmPosition == 0)
+    currentArmPosition = armPositionDown;
+  
+  for(int i = armPositionDown; i < armPositionUp; i++){
+    addArmIncrement(i, 1);
   }
 }
 
+void addClawIncrement(int increment){
+   if(increment > clawPositionClosed || increment < clawPositionOpen)
+    return;
+      
+   clawServo.writeMicroseconds(increment);
+   currentClawPosition = increment;
+   delay(5);
+}
+
 void openClaw(){
-  for(int i = clawPositionClosed; i > clawPositionOpen; i--){
-    clawServo.writeMicroseconds(i);
-    delay(5);
+  for(int i = currentClawPosition; i > clawPositionOpen; i--){
+    addClawIncrement(i);
   }
 }
 
 void closeClaw(){
-  for(int i = clawPositionOpen; i < clawPositionClosed; i++){
-    clawServo.writeMicroseconds(i);
-    delay(5);
+  if(currentClawPosition == 0)
+    currentClawPosition = clawPositionOpen;
+    
+  for(int i = currentClawPosition; i < clawPositionClosed; i++){
+    addClawIncrement(i);
   }
 }
